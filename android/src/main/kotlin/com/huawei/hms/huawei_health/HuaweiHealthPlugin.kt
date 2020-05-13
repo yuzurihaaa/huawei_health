cafc24b5b3f2eaffeb35f2764df0169182e5d271
yuzuriha <zaralockheart@gmail.com>
2020-05-13 19:08:53 +0800

    improvise plugins with reference of https://github.com/Baseflow/flutter-permission-handler/blob/develop/permission_handler/android/src/main/java/com/baseflow/permissionhandler/PermissionHandlerPlugin.java

--- android/src/main/kotlin/com/huawei/hms/huawei_health/HuaweiHealthPlugin.kt
+++ android/src/main/kotlin/com/huawei/hms/huawei_health/HuaweiHealthPlugin.kt
@@ -2,66 +2,82 @@ package com.huawei.hms.huawei_health
 
 import android.app.Activity
 import androidx.annotation.NonNull
-import com.huawei.hms.huawei_health.scenes.getDistance
-import com.huawei.hms.huawei_health.scenes.getSteps
-import com.huawei.hms.huawei_health.scenes.hmsLogin
+import com.huawei.hms.huawei_health.scenes.HmsLoginScene
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** HuaweiHealthPlugin */
-class HuaweiHealthPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
-    private lateinit var activity: Activity
+/** HuaweiHealthPlugin */
+class HuaweiHealthPlugin : FlutterPlugin, ActivityAware {
+
+    private var hmsLoginScene: HmsLoginScene? = null
+
+    private var methodChannel: MethodChannel? = null
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        val channel = MethodChannel(flutterPluginBinding.binaryMessenger, "huawei_health")
-        channel.setMethodCallHandler(this)
+        startListening(flutterPluginBinding.binaryMessenger)
     }
 
 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "huawei_health")
-            channel.setMethodCallHandler(HuaweiHealthPlugin())
-        }
-    }
-
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        when (call.method) {
-            "loginHuawei" -> {
-                hmsLogin(activity, result)
+            val plugin = HuaweiHealthPlugin()
+            if (registrar.activeContext() is Activity) {
+                plugin.startListeningToActivity(
+                        registrar.activity()
+                );
             }
-            "getSteps" -> {
-                getSteps(call, activity, result)
-            }
-            "getDistance" -> {
-                getDistance(call, activity, result)
-            }
-            else -> result.notImplemented()
         }
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        stopListening()
     }
 
     override fun onDetachedFromActivity() {
+        stopListeningToActivity()
     }
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        this.activity = binding.activity
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        this.activity = binding.activity
+        startListeningToActivity(binding.activity)
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    private fun startListening(messenger: BinaryMessenger) {
+        methodChannel = MethodChannel(
+                messenger,
+                "huawei_health")
+        hmsLoginScene = HmsLoginScene()
+        methodChannel?.setMethodCallHandler(hmsLoginScene)
+    }
+
+    private fun startListeningToActivity(
+            activity: Activity
+    ) {
+        if (hmsLoginScene != null) {
+            hmsLoginScene?.setActivity(activity)
+        }
+    }
+
+    private fun stopListeningToActivity() {
+        if (hmsLoginScene != null) {
+            hmsLoginScene?.setActivity(null)
+        }
+    }
+
+    private fun stopListening() {
+        methodChannel?.setMethodCallHandler(null)
+        methodChannel = null
+        hmsLoginScene = null
     }
 }
--- android/src/main/kotlin/com/huawei/hms/huawei_health/scenes/HmsLoginScene.kt
+++ android/src/main/kotlin/com/huawei/hms/huawei_health/scenes/HmsLoginScene.kt
@@ -2,6 +2,7 @@ package com.huawei.hms.huawei_health.scenes
 
 import android.app.Activity
 import android.util.Log
+import androidx.annotation.NonNull
 import com.huawei.hihealthkit.HiHealthDataQuery
 import com.huawei.hihealthkit.HiHealthDataQueryOption
 import com.huawei.hihealthkit.auth.HiHealthAuth
@@ -24,97 +25,121 @@ private const val TAG = "HmsLoginScene"
 private const val ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSS"
 private const val TIMEOUT = 0
 
-fun hmsLogin(activity: Activity, result: MethodChannel.Result) {
-    val read = intArrayOf(
-            HiHealthOpenPermissionType.HEALTH_OPEN_PERMISSION_TYPE_READ_DATA_POINT_STEP_SUM,
-            HiHealthOpenPermissionType.HEALTH_OPEN_PERMISSION_TYPE_READ_DATA_POINT_DISTANCE_SUM)
-    val write = intArrayOf()
+class HmsLoginScene: MethodChannel.MethodCallHandler {
 
-    HiHealthAuth.requestAuthorization(activity, write, read) { code, reason ->
-        Log.i(TAG, "requestAuthorization onResult: $code")
-        Log.i(TAG, "requestAuthorization onResult: $reason")
-        activity.runOnUiThread {
-            if (code != 0) {
-                result.success("Opening authorization screen")
+    private var activity: Activity? = null
+
+    fun setActivity(activity: Activity?) {
+        this.activity = activity
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+        when (call.method) {
+            "loginHuawei" -> {
+                hmsLogin(result)
+            }
+            "getSteps" -> {
+                getSteps(call, result)
+            }
+            "getDistance" -> {
+                getDistance(call, result)
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    private fun hmsLogin(result: MethodChannel.Result) {
+        val read = intArrayOf(
+                HiHealthOpenPermissionType.HEALTH_OPEN_PERMISSION_TYPE_READ_DATA_POINT_STEP_SUM,
+                HiHealthOpenPermissionType.HEALTH_OPEN_PERMISSION_TYPE_READ_DATA_POINT_DISTANCE_SUM)
+        val write = intArrayOf()
+
+        HiHealthAuth.requestAuthorization(activity, write, read) { code, reason ->
+            Log.i(TAG, "requestAuthorization onResult: $code")
+            Log.i(TAG, "requestAuthorization onResult: $reason")
+            activity?.runOnUiThread {
+                if (code != 0) {
+                    result.success("Opening authorization screen")
+                } else {
+                    result.success("Opening authorization screen")
+                }
+            }
+
+        }
+    }
+
+    private fun getSteps(call: MethodCall, result: MethodChannel.Result) {
+        val (startTime, endTime) = getTime(call)
+
+        val hiHealthDataQuery = HiHealthDataQuery(HiHealthPointType.DATA_POINT_STEP_SUM, startTime,
+                endTime, HiHealthDataQueryOption())
+
+        try {
+            runBlocking {
+                val data = queryHiHealth(hiHealthDataQuery)
+
+                activity?.runOnUiThread {
+                    result.success(data)
+                }
+            }
+
+        } catch (e: HiHealthException) {
+            result.error(e.code.toString(), e.message, null)
+        }
+    }
+
+    private fun getDistance(call: MethodCall, result: MethodChannel.Result) {
+        val (startTime, endTime) = getTime(call)
+
+        val hiHealthDataQuery = HiHealthDataQuery(HiHealthPointType.DATA_POINT_DISTANCE_SUM, startTime,
+                endTime, HiHealthDataQueryOption())
+
+        try {
+            runBlocking {
+                val data = queryHiHealth(hiHealthDataQuery)
+
+                activity?.runOnUiThread {
+                    result.success(data)
+                }
+            }
+
+        } catch (e: HiHealthException) {
+            result.error(e.code.toString(), e.message, null)
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    suspend fun queryHiHealth(query: HiHealthDataQuery) = suspendCoroutine<Map<String, Int>> { cont ->
+        HiHealthDataStore.execQuery(activity, query, TIMEOUT) { resultCode, data ->
+            Log.i(TAG, "result code $resultCode")
+            if (data != null && data is ArrayList<*>) {
+                val dataMap = mutableMapOf<String, Int>()
+                val dataList: List<HiHealthPointData> = data as List<HiHealthPointData>
+                for (obj in dataList) {
+                    val calendar = Calendar.getInstance()
+                    calendar.timeInMillis = obj.startTime
+
+                    val dateFormat = SimpleDateFormat(ISO_FORMAT, Locale.US)
+                    dataMap[dateFormat.format(calendar.time)] = obj.value
+
+                }
+                Log.i(TAG, "result $dataMap")
+                cont.resume(dataMap.toMap())
             } else {
-                result.success("Opening authorization screen")
+                cont.resumeWithException(HiHealthException(resultCode, "Data is null"))
             }
         }
-
     }
-}
 
-fun getSteps(call: MethodCall, activity: Activity, result: MethodChannel.Result) {
-    val (startTime, endTime) = getTime(call)
+    private fun getTime(call: MethodCall): Pair<Long, Long> {
 
-    val hiHealthDataQuery = HiHealthDataQuery(HiHealthPointType.DATA_POINT_STEP_SUM, startTime,
-            endTime, HiHealthDataQueryOption())
+        Log.i(TAG, "result code ${call.arguments}")
+        val endTime = call.argument<String>("endTime")
+        val endTimeInLong = SimpleDateFormat(ISO_FORMAT, Locale.US).parse(endTime)
 
-    try {
-        runBlocking {
-            val data = queryHiHealth(activity, hiHealthDataQuery)
+        val startTime = call.argument<String>("startTime")
+        val startTimeInLong = SimpleDateFormat(ISO_FORMAT, Locale.US).parse(startTime)
 
-            activity.runOnUiThread {
-                result.success(data)
-            }
-        }
-
-    } catch (e: HiHealthException) {
-        result.error(e.code.toString(), e.message, null)
+        return Pair(startTimeInLong.time, endTimeInLong.time)
     }
-}
-
-fun getDistance(call: MethodCall, activity: Activity, result: MethodChannel.Result) {
-    val (startTime, endTime) = getTime(call)
-
-    val hiHealthDataQuery = HiHealthDataQuery(HiHealthPointType.DATA_POINT_DISTANCE_SUM, startTime,
-            endTime, HiHealthDataQueryOption())
-
-    try {
-        runBlocking {
-            val data = queryHiHealth(activity, hiHealthDataQuery)
-
-            activity.runOnUiThread {
-                result.success(data)
-            }
-        }
-
-    } catch (e: HiHealthException) {
-        result.error(e.code.toString(), e.message, null)
-    }
-}
-
-@Suppress("UNCHECKED_CAST")
-suspend fun queryHiHealth(activity: Activity, query: HiHealthDataQuery) = suspendCoroutine<Map<String, Int>> { cont ->
-    HiHealthDataStore.execQuery(activity, query, TIMEOUT) { resultCode, data ->
-        Log.i(TAG, "result code $resultCode")
-        if (data != null && data is ArrayList<*>) {
-            val dataMap = mutableMapOf<String, Int>()
-            val dataList: List<HiHealthPointData> = data as List<HiHealthPointData>
-            for (obj in dataList) {
-                val calendar = Calendar.getInstance()
-                calendar.timeInMillis = obj.startTime
-
-                val dateFormat = SimpleDateFormat(ISO_FORMAT, Locale.US)
-                dataMap[dateFormat.format(calendar.time)] = obj.value
-
-            }
-            Log.i(TAG, "result $dataMap")
-            cont.resume(dataMap.toMap())
-        } else {
-            cont.resumeWithException(HiHealthException(resultCode, "Data is null"))
-        }
-    }
-}
-
-fun getTime(call: MethodCall): Pair<Long, Long> {
-
-    Log.i(TAG, "result code ${call.arguments}")
-    val endTime = call.argument<String>("endTime")
-    val endTimeInLong = SimpleDateFormat(ISO_FORMAT, Locale.US).parse(endTime)
-
-    val startTime = call.argument<String>("startTime")
-    val startTimeInLong = SimpleDateFormat(ISO_FORMAT, Locale.US).parse(startTime)
-
-    return Pair(startTimeInLong.time, endTimeInLong.time)
 }
